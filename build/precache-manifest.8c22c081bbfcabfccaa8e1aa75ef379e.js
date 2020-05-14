f6084cf2565be57aa1923be29e3d973a7c0a8ec2
Tamara Sidorova <tomatoma891@gmail.com>
2020-05-13 21:05:05 -0600

    added visuals

---
+++ build/precache-manifest.8c22c081bbfcabfccaa8e1aa75ef379e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6afe595ba6214fdd8b9da4c104ccd6",
+    "url": "/company-directory/index.html"
+  },
+  {
+    "revision": "d3d67025686f25d76842",
+    "url": "/company-directory/static/css/main.8e10ea49.chunk.css"
+  },
+  {
+    "revision": "46d1030cb5e326514868",
+    "url": "/company-directory/static/js/2.2a09ebe7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/company-directory/static/js/2.2a09ebe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d67025686f25d76842",
+    "url": "/company-directory/static/js/main.320e0363.chunk.js"
+  },
+  {
+    "revision": "18951206d49098d2ac7b",
+    "url": "/company-directory/static/js/runtime-main.7105e178.js"
+  },
+  {
+    "revision": "2d4c55904f4d16d126908e32fef8ef86",
+    "url": "/company-directory/static/media/office.2d4c5590.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employee-directory/precache-manifest.e6450474807c9355c80788455fc92b4d.js"
+  "/company-directory/precache-manifest.8c22c081bbfcabfccaa8e1aa75ef379e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/company-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
